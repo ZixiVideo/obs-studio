ab32071e3cbd4db226d2d8d7824d84d6fd9a5294
Roy Jacober <roy@zixi.com>
2020-06-14 11:13:42 +0300

    [UI] - change download link for zixi feeder

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -7581,25 +7581,18 @@ void OBSBasic::on_actionShowAbout_triggered()
 
 void OBSBasic::on_actionGetZixi_triggered()
 {
-#ifdef WIN32
-#if ARCH_BITS == 64
-	QUrl zixi_url("http://downloads.zixi.com/free/feeder_interface/windows/windows_download.html");
-#else // ARCH_BITS
-	QUrl zixi_url("http://downloads.zixi.com/free/feeder_interface/windows/windows32_download.html");
-#endif // ARCH_BITS
-#elif __APPLE__
-	QUrl zixi_url("http://downloads.zixi.com/free/feeder_interface/osx/osx_download.html");
-#else
-	QUrl zixi_url("http://downloads.zixi.com/free/feeder_interface/linux/linux_download.html");
-#endif
+	QUrl zixi_url("https://portal.zixi.com/self-registration/OBS_STUDIO_ZIXI_FEEDER");
+
 	int failed = -1;
 	if (QDesktopServices::openUrl(zixi_url)) {
 		failed = 0;
 	} else {
 		failed = 1;
 	}
-
-	int m = failed;
+	
+	if (failed != 0) {
+		blog(LOG_ERROR, "Failed to open browser for zixi feeder download");
+	}
 }
 
 void OBSBasic::ResizeOutputSizeOfSource()
