e4fc30b40028fff8b6d26306eed4b23122a5467b
Roy Jacober <roy@zixi.com>
2020-10-29 11:43:55 +0200

    zixi portal url changes

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -7955,7 +7955,7 @@ void OBSBasic::on_actionShowAbout_triggered()
 
 void OBSBasic::on_actionGetZixi_triggered()
 {
-	QUrl zixi_url("https://portal.zixi.com/self-registration/OBS_STUDIO_ZIXI_FEEDER?build=721");
+	QUrl zixi_url("https://portal.zixi.com/self-registration/OBS_STUDIO_ZIXI_FEEDER?build=obs_plugin&os=windows64");
 
 	int failed = -1;
 	if (QDesktopServices::openUrl(zixi_url)) {
